4a5420e6d86a8d7525948f5041d150f2a424191c
Mohamed Abdullah <63874767+mohamed-abdullah1@users.noreply.github.com>
2023-08-06 13:04:55 +0300

    strucutre the scss styles and folders

---
+++ src/features/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./styles/home.module.scss";
+const Home = () => {
+  return (
+    <div>
+      <h1 style={styles.h1}>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
